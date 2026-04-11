b211cabd0f1a8282e76bb9a2731ea3bce71b3ee4
Sebastien Dunne <dunn0203@algonquinlive.com>
2026-04-11 18:24:07 -0400

    refactored backend to use iot hub

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,16 +1,33 @@
-from azure.messaging.webpubsubservice import WebPubSubServiceClient
 import os
 import azure.functions as func
+import time
+import urllib.parse
+import hmac
+import hashlib
+import base64
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.route(route="get_access_token")
-def get_access_token(req: func.HttpRequest) -> func.HttpResponse:
-    service = WebPubSubServiceClient.from_connection_string(
-        os.environ["WPS_CONNECTION_STRING"], hub="led"
+@app.post(route="generate_sas_token")
+def generate_sas_token(req):
+    ttl = int(time.time() + req.params.get("expiry"))
+    uri = req.params.get("uri")
+    sign_key = f"{urllib.parse.quote(uri)}\n{ttl}".encode("utf-8")
+
+    signature = base64.b64encode(
+        hmac.new(
+            base64.b64decode(req.params.get("key")),
+            sign_key,
+            hashlib.sha256
+        ).digest()
     )
 
-    token = service.get_client_access_token(user_id="esp32-device")
+    token = (
+        f"SharedAccessSignature sr={urllib.parse.quote(uri)}"
+        f"&sig={urllib.parse.quote(signature)}"
+        f"&se={ttl}"
+        f"&skn={req.params.get("policy_name")}"
+    )
 
-    return func.HttpResponse(token["url"], mimetype="text/plain")
+    return token
