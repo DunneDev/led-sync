c3f54bb2e5dc7be1ed6e9f4686663c2dc79c7439
Sebastien Dunne <dunn0203@algonquinlive.com>
2026-04-12 07:26:00 -0400

    updated backend to only need auth to generate SAS token

--- backend/function_app.py
+++ backend/function_app.py
@@ -9,15 +9,15 @@ import base64
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.post(route="generate_sas_token")
+@app.route(route="generate_sas_token")
 def generate_sas_token(req):
-    ttl = int(time.time() + req.params.get("expiry"))
-    uri = req.params.get("uri")
+    ttl = int(time.time() + 3600)
+    uri = f"{os.getenv("IOT_HUB_NAME")}.azure-devices.net/{os.getenv("DEVICE_NAME")}/?api-version=2021-04-12"
     sign_key = f"{urllib.parse.quote(uri)}\n{ttl}".encode("utf-8")
 
     signature = base64.b64encode(
         hmac.new(
-            base64.b64decode(req.params.get("key")),
+            base64.b64decode(os.getenv("IOT_HUB_KEY")),
             sign_key,
             hashlib.sha256
         ).digest()
@@ -25,9 +25,9 @@ def generate_sas_token(req):
 
     token = (
         f"SharedAccessSignature sr={urllib.parse.quote(uri)}"
-        f"&sig={urllib.parse.quote(signature)}"
+        f"&sig={urllib.parse.quote(signature.decode())}"
         f"&se={ttl}"
-        f"&skn={req.params.get("policy_name")}"
+        f"&skn=iothubowner"
     )
 
     return token
